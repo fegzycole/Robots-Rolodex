085a4f6f4f4cde19acfaabeaa08e1bbc6ff2d87f
fegzycole <fergusoniyara@gmail.com>
2020-01-25 19:38:28 +0100

    Updates

---
+++ precache-manifest.50ace6c99e9aeaab0f1b2dcac8304bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c149d36c3b214a40abfcb68b51dbaa",
+    "url": "/Robots-Rolodex/index.html"
+  },
+  {
+    "revision": "02571093c93c2ccd14fd",
+    "url": "/Robots-Rolodex/static/css/main.7d773e33.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/Robots-Rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robots-Rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02571093c93c2ccd14fd",
+    "url": "/Robots-Rolodex/static/js/main.969f70a1.chunk.js"
+  },
+  {
+    "revision": "9a5e71a54c2cc46abfa8",
+    "url": "/Robots-Rolodex/static/js/runtime-main.ec2fc1c8.js"
+  }
+]);
